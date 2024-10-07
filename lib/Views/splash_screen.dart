a3ac6227458ffabec2b7e6cca68500e1db154ff7
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-07 09:33:24 +0600

    splash screen done

---
+++ lib/Views/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../Utils/app_assets.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 3), () {
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+            (route) {
+          return false;
+        });
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                AppAssets.splashLogo,
+                height: 130,
+                width: 130,
+              ),
+              const Text(
+                "Daily To-Do App",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
